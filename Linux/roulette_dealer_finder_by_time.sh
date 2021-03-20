a8a71ffdf83f1aa3654a1e5cef05bd0c84ce4cb1
Elizabeth Johnson <bethwest@protonmail.com>
2021-03-20 09:54:00 -0500

    minor correction for typo

--- Linux/roulette_dealer_finder_by_time.sh
+++ Linux/roulette_dealer_finder_by_time.sh
@@ -3,6 +3,6 @@
 #$2 is time in 00:00:00 AM/PM format. Formatting is critical - see note below:
 #NOTE:  MUST input argument in this format: 00:00:00[[:space:]]AM or 00:00:00[[:space:]]PM
 
-#! bin/bash
+#!/bin/bash
 
 grep -i $2 $1_Dealer_schedule | awk -F" " '{print $1, $2, $5, $6}'
