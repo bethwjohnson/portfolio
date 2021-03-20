f6aa659098c3bb73f44d69630fc8b31cc7a11411
Elizabeth Johnson <bethwest@protonmail.com>
2021-03-20 10:25:29 -0500

    minor corrections

--- Linux/system.sh
+++ Linux/system.sh
@@ -4,14 +4,14 @@
 
 free -h > ~/backups/freemem/free_mem.txt
 
-#prints disk usage and saves to disk_usage.txt
+# prints disk usage and saves to disk_usage.txt
 
 du -h > ~/backups/diskuse/disk_usage.txt
 
-#lists all open files and saves to open_lists.txt
+# lists all open files and saves to open_lists.txt
 
 ps -aux > ~/backups/openlist/open_list.txt
 
-#prints file system disk space statistics and saves to free_disk.txt
+# prints file system disk space statistics and saves to free_disk.txt
 
 df -h > ~/backups/freedisk/free_disk.txt
